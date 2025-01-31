81df3cf79faebbca917f1180643168c6317dc973
x0x0b <42596409+x0x0b@users.noreply.github.com>
2025-02-01 08:33:08 +0900

    Initial commit

---
+++ src/main/java/com/x0x0b/piyo_todo_api/PiyoTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.x0x0b.piyo_todo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiyoTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiyoTodoApiApplication.class, args);
+	}
+
+}
