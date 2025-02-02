84c977026653d2f2ab3c75fb23b54cc41dfe0fe5
x0x0b <42596409+x0x0b@users.noreply.github.com>
2025-02-02 22:01:31 +0900

    404

--- src/main/java/com/x0x0b/piyo_todo_api/handler/ExceptionHandlerAdvice.java
+++ src/main/java/com/x0x0b/piyo_todo_api/handler/ExceptionHandlerAdvice.java
@@ -5,10 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionHandlerAdvice {
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ResponseEntity<Object> handle(NoResourceFoundException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+  }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<Object> handle(Exception e) {
