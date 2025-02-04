1a0f4cb6e7bf1363db7c36d916a058c07410bdf6
x0x0b <42596409+x0x0b@users.noreply.github.com>
2025-02-04 22:14:53 +0900

    presigned url 用のIAM(key,secret)を使うように修正

--- src/main/java/com/x0x0b/piyo_todo_api/conf/S3ClientConfiguration.java
+++ src/main/java/com/x0x0b/piyo_todo_api/conf/S3ClientConfiguration.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -45,10 +46,12 @@ public class S3ClientConfiguration {
 
   @Bean
   public S3Presigner s3Presigner() {
-    AwsCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+    AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+    );
     return S3Presigner.builder()
         .region(region)
-        .credentialsProvider(() -> credentials)
+        .credentialsProvider(credentialsProvider)
         .build();
   }
 }
