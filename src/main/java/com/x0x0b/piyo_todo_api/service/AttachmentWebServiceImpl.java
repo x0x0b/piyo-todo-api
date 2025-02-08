d171fdfebd9ba7c56b09a06882c48accc60310cb
x0x0b <42596409+x0x0b@users.noreply.github.com>
2025-02-08 13:44:20 +0900

    add @Transactional

--- src/main/java/com/x0x0b/piyo_todo_api/service/AttachmentWebServiceImpl.java
+++ src/main/java/com/x0x0b/piyo_todo_api/service/AttachmentWebServiceImpl.java
@@ -4,6 +4,7 @@ import com.x0x0b.piyo_todo_api.domain.AttachmentWeb;
 import com.x0x0b.piyo_todo_api.repository.AttachmentWebRepository;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AttachmentWebServiceImpl implements AttachmentWebService {
@@ -15,11 +16,13 @@ public class AttachmentWebServiceImpl implements AttachmentWebService {
   }
 
   @Override
+  @Transactional(readOnly = true)
   public List<AttachmentWeb> getList(Long todoId) {
     return attachmentWebRepository.getList(todoId);
   }
 
   @Override
+  @Transactional
   public void add(AttachmentWeb attachmentWeb) {
     attachmentWebRepository.insert(attachmentWeb.getTodoId(), attachmentWeb.getUrl());
   }
