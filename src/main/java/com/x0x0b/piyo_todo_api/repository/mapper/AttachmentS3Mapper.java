d35834840d817c46018cfc20d992794653b81208
x0x0b <42596409+x0x0b@users.noreply.github.com>
2025-02-08 13:49:16 +0900

    add @Mapper

--- src/main/java/com/x0x0b/piyo_todo_api/repository/mapper/AttachmentS3Mapper.java
+++ src/main/java/com/x0x0b/piyo_todo_api/repository/mapper/AttachmentS3Mapper.java
@@ -2,7 +2,9 @@ package com.x0x0b.piyo_todo_api.repository.mapper;
 
 import com.x0x0b.piyo_todo_api.domain.AttachmentS3;
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AttachmentS3Mapper {
 
   List<AttachmentS3> getByTodoId(Long todoId);
--- src/main/java/com/x0x0b/piyo_todo_api/repository/mapper/AttachmentWebMapper.java
+++ src/main/java/com/x0x0b/piyo_todo_api/repository/mapper/AttachmentWebMapper.java
@@ -2,7 +2,9 @@ package com.x0x0b.piyo_todo_api.repository.mapper;
 
 import com.x0x0b.piyo_todo_api.domain.AttachmentWeb;
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AttachmentWebMapper {
 
   List<AttachmentWeb> getByTodoId(Long todoId);
